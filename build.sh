b731fc549f20afac2c7f6c60361a68bc4b1dbe3b
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-06-07 02:13:24 -0700

    Run after building

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
 export DEVELOPER_DIR=/Applications/Xcode-beta.app/Contents/Developer
 SWIFTFLAGS="-sdk /Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator13.0.sdk -target x86_64-apple-ios13.0-simulator"
-exec swiftc $SWIFTFLAGS -o marina main.swift marina.swift marina_html.swift $@
+swiftc $SWIFTFLAGS -o marina main.swift marina.swift marina_html.swift $@
+./run.sh
